b13b684313db6e0a2f5885cb43af213eab4daeeb
Nicholas-Alves <41400491+Nicholas-Alves@users.noreply.github.com>
2021-02-23 14:18:18 -0300

    add: TypeORM; User: model, controller, create route.

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,12 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-//http://localhost:3333/users
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World - NLW04" });
-});
-
-// 1 param => Rota (Recurso API)
-// 2 param => request, response
-
-app.post("/", (req, res) => {
-  // Recebeu os dados para salvar
-  return res.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json())
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
