bdfe8ea061fedd7abe33e0c7e73d01ea9f289157
Nicholas-Alves <41400491+Nicholas-Alves@users.noreply.github.com>
2021-02-26 12:39:42 -0300

    add: Answer: controller and route

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+class AnswerController {
+  //http://localhost:3333/answers/1?u=47ebc6e7-eec4-4c48-bbc5-43c62a4fa291
+  async execute(req: Request, res: Response) {
+    const { value } = req.params;
+    const { u } = req.query;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveyUser = await surveysUsersRepository.findOne({
+      id: String(u),
+    });
+
+    if (!surveyUser)
+      return res.json({
+        error: "Survey User does not exists.",
+      });
+
+    surveyUser.value = Number(value);
+
+    await surveysUsersRepository.save(surveyUser);
+
+    return res.json(surveyUser);
+  }
+}
+
+export { AnswerController };
